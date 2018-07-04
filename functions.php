331678e5f99e6fb39d9b17e909b4357af525c1bb
LucilaG <37339182+LucilaG@users.noreply.github.com>
2018-07-04 13:21:52 -0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+add_action('after_setup_theme', 'my_theme_theme_setup');
+
+if ( ! function_exists( 'my_theme_theme_setup' ) ){
+    function my_theme_theme_setup(){
+
+        add_action('init', 'my_theme_register_my_menus');
+
+        if ( ! function_exists( 'my_theme_register_my_menus' ) ){
+            function my_theme_register_my_menus(){
+                register_nav_menus(array(
+                    'header-menu'  => esc_html__('Header Menu', 'my_theme'),
+                    'footer-menu'  => esc_html__('Footer Menu', 'my_theme'),
+                ));
+            }
+        }
+    }
+}
+
+function learningWordpress_resources(){
+
+    wp_enqueue_style('style',get_stylesheet_uri());
+
+}
+add_action('wp_enqueue_scripts','learningWordpress_resources')
+
+
+?>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php bloginfo('name');?></title>
+    <link href='https://fonts.googleapis.com/css?family=Anaheim' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>> 
+    <div class="container">
+        <header class="site-header">
+            <h1 class="text-center"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+            <h5 class="text-center"><?php bloginfo('description'); ?></h5>
+            <nav class="site-nav">
+            <?php wp_nav_menu(array('theme_location' => 'header-menu'));?>﻿
+            </nav>
+        </header>
