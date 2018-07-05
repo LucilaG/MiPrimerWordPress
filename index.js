b3bb8e5701e1c18d1999075962732d9a355bac45
LucilaG <37339182+LucilaG@users.noreply.github.com>
2018-07-05 13:01:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     "use stric";
     function adding_classes(){
-        $(".nav_child_page > ul").addClass("nav");
+        $(".nav_child_page > ul").addClass("nav nav-tabs");
         $(".nav_child_page > ul > li").addClass("nav-item");
         $(".nav_child_page > ul > li > a").addClass("nav-link");
     }
