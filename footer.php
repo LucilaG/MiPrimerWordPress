e128d10a5eb51a6c5a7b9802563c3e0462281240
LucilaG <37339182+LucilaG@users.noreply.github.com>
2018-07-06 11:11:21 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 </div>
-       <footer class= "site-footer text-center container">
+       <footer class= "site-footer container-fluid">
             <nav class="site-nav row">
                 <div class="col-12">
                 <?php wp_nav_menu(array('theme_location' => 'footer-menu'));?>
                 </div>
             </nav>
             <div class="row">
-                 <p class="col-12"><span><?php bloginfo('name'); ?></span> - &copy; <?php echo date('Y'); ?></p>
+                 <p class="col-12 text-center"><?php bloginfo('name'); ?>- &copy; <?php echo date('Y'); ?></p>
             </div>
         </footer>
 
--- header.php
+++ header.php
@@ -5,15 +5,13 @@
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php bloginfo('name');?></title>
-    <link href='https://fonts.googleapis.com/css?family=Anaheim' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Artifika' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Cutive Mono' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Homemade Apple' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>> 
-    <div class="container">
+    <div class="container-fluid">
         <header class="site-header">
             <h1 class="text-center"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
             <h5 class="text-center"><?php bloginfo('description'); ?></h5>
